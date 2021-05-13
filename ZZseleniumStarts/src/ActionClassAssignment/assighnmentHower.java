bfb9640025b2997ca2331f6ed3e0211216340325
akshay <akshay@Akshay>
2021-05-13 13:52:02 +0530

    asighnment

---
+++ ZZseleniumStarts/src/ActionClassAssignment/assighnmentHower.java
@@ -0,0 +1,39 @@
+package ActionClassAssignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class assighnmentHower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\akshay\\Downloads\\selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
+		driver.navigate().refresh();
+		Actions act=new Actions(driver);
+		
+		WebElement username=driver.findElement(By. xpath("//*[@name='txtUsername']"));
+		username.sendKeys("Admin");
+		act.sendKeys(username, Keys.TAB).sendKeys("admin123").sendKeys(username, Keys.ENTER).build().perform();
+		
+		WebElement hOwer=driver.findElement(By.linkText("Admin"));
+		act.moveToElement(hOwer).perform();
+		
+		WebElement user=driver.findElement(By.xpath("//*[@id='menu_admin_UserManagement']"));
+		act.moveToElement(user).perform();
+		
+		
+		WebElement userlast=driver.findElement(By.xpath("//*[@id='menu_admin_viewSystemUsers']"));
+		act.moveToElement(userlast).click(userlast).perform();
+		
+		
+		
+	}
+
+}
---
+++ ZZseleniumStarts/src/ActionClassAssignment/google.java
@@ -0,0 +1,29 @@
+package ActionClassAssignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\akshay\\Downloads\\selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.navigate().refresh();
+	Actions act=new Actions(driver);
+		
+		WebElement printGoogle=driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+		act.sendKeys("god pics").sendKeys(printGoogle, Keys.DOWN).sendKeys(printGoogle, Keys.DOWN).sendKeys(printGoogle, Keys.DOWN).click(printGoogle).build().perform();
+		 
+		WebElement enter=driver.findElement(By.xpath("(//*[@class='gNO89b'])[2]"));
+		act.click(enter).perform();
+
+}
+}
