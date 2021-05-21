fd36cdc730d4fb9dd128814b618ab74d674e9539
akshay <akshay@Akshay>
2021-05-21 15:27:09 +0530

    aakshay

--- ZZseleniumStarts/src/FileRandom/RandomDataChange.java
+++ ZZseleniumStarts/src/FileRandom/RandomDataChange.java
@@ -6,26 +6,45 @@ import java.io.FileNotFoundException;
 import java.util.Properties;
 
 import org.apache.poi.hwpf.usermodel.ParagraphProperties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RandomDataChange {
 
 	public static void main(String[] args) throws Throwable {
-		// TODO Auto-generated method stub
-		File file=new File(System.getProperty("user.dir")+"\\config.properties");
-FileInputStream fis= new FileInputStream(file);
-//abhitak handle kiya h abhi read krna h
-Properties prop=new Properties();
-prop.load(fis);
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\akshay\\Downloads\\selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 
-   String value=prop.getProperty("testsiteurl");
-   System.out.println(value);
-   
-   String id=prop.getProperty("username");
-   System.out.println(id);
-   
-   String password1=prop.getProperty("password");
-   System.out.println(password1);
-   
+		File file = new File(System.getProperty("user.dir") + "\\config.properties");
+		FileInputStream fis = new FileInputStream(file);
+		Properties prop = new Properties();
+		prop.load(fis);
+
+		String value = prop.getProperty("testsiteurl");
+		System.out.println(value);
+
+		String id = prop.getProperty("username");
+		System.out.println(id);
+
+		String password1 = prop.getProperty("password");
+		System.out.println(password1);
+
+		
+
+		driver.get(prop.getProperty("testsiteurl"));
+		WebElement username = driver.findElement(By.id("txtUsername"));
+		username.sendKeys(prop.getProperty("username"));
+
+		WebElement Password = driver.findElement(By.id("txtPassword"));
+		Password.sendKeys(prop.getProperty("password"));
+
+		WebElement submit = driver.findElement(By.name("Submit"));
+		Thread.sleep(10000);
+		submit.click();
+		driver.close();
+
 	}
 
 }
